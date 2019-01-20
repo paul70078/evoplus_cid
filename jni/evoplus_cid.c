5dab1b7ffcbd56f25356302d81a84dede2ec8a5d
Paul Götzinger <paul70079@gmail.com>
2019-01-20 15:57:19 +0100

    more fixes

--- jni/evoplus_cid.c
+++ jni/evoplus_cid.c
@@ -31,32 +31,34 @@ int mmc_cmd(int fd, unsigned int opcode, unsigned int arg, int flags, char const
 	return ret;
 }
 
-int mmc_change_lock(int fd, int lock, char const*const password) {
-	char data[500];
+int mmc_change_lock(int fd, int lock, char const*const password, int len) {
+	char data[20];
 
-	//set block size
-	int ret = mmc_cmd(fd, MMC_CMD_BLKL, 512, MMC_RSP_R1 | MMC_RSP_SPI_R1 | MMC_CMD_AC, 0, 0);
+	if (len < 1 || len > 16) {
+		printf("Invalid password length: %d\n", len);
+		return 0;
+	}
+
+	int blocklen = len + 2;
+
+
+	//set block length
+	int ret = mmc_cmd(fd, MMC_CMD_BLKL, blocklen, MMC_RSP_R1 | MMC_RSP_SPI_R1 | MMC_CMD_AC, 0, 0);
 
 	if (ret) {
 		printf("set block length failed: %d\n", ret);
 		return ret;
 	}
 	
-	char len = strlen(password);
 	
 	data[0] = 0xFE;
-	data[1] = lock ? 0x01 : 0x02;
+	data[1] = lock ? 0x05 : 0x02;
 	data[2] = len;
 	memcpy(&(data[3]), password, len);
 	data[len+3] = 0xff;
 	data[len+4] = 0xff;
 
 	ret = mmc_cmd(fd, MMC_CMD_LOCK, 0, MMC_RSP_R1 | MMC_RSP_SPI_R1 | MMC_CMD_ADTC, data, len+5);	//set password
-
-	if (ret && lock) {
-		data[1] = 0x04;
-		mmc_cmd(fd, MMC_CMD_LOCK, 0, MMC_RSP_R1 | MMC_RSP_SPI_R1 | MMC_CMD_ADTC, data, len+5);	//set password
-	}
 
 	if (!ret) {
 		printf("lock/unlock failed: %d\n", ret);
@@ -65,8 +67,45 @@ int mmc_change_lock(int fd, int lock, char const*const password) {
 	return ret;
 }
 
+static int charToHex(char c) {
+	if (c >= '0' && c <= '9') {
+		return c - '0';
+	} else if (c >= 'a' && c <= 'f') {
+		return c - 'a' + 0xa;
+	} else if (c >= 'A' && c <= 'F') {
+		return c - 'A' + 0xA;
+	} else {
+		printf("Invalid character %c 0x%02.2x\n", c, c);
+		return 0;
+	}
+}
+
+static int readHex(char const * const in, char * const out, int olen) {
+	int ilen = strlen(in);
+
+	if ((olen << 1) < ilen) {
+		printf("Hex output not large enough");
+		return 0;
+	}
+
+	int len = 0;
+
+	char const * ptr = in;
+
+	while (*ptr) {
+		out[len] = charToHex(*(ptr++));
+
+		if (len > 0 || (ilen%2 == 0)) {
+			out[len] = (out[len] << 4) + charToHex(*(ptr++));
+		}
+
+		len++;
+	}
+
+	return len;
+}
+
 int main(int argc, const char **argv) {
-	int fd, ret, i, len;
 
 	if (argc != 4) { //check parameter length
 		printf("Invalid parameter count\n");
@@ -74,19 +113,22 @@ int main(int argc, const char **argv) {
 	}
 
 	// open device
-	fd = open(argv[2], O_RDWR);
+	int fd = open(argv[2], O_RDWR);
 	if (fd < 0){
 		printf("Unable to open device %s\n", argv[2]);
 		return -1;
 	}
 
+	char password[100];
+	int len = readHex(argv[3], password, 100);
+
 	//lock/unlock
 	if (strcmp(argv[1], "lock") == 0) {\
 		printf("trying to lock...\n");
-		mmc_change_lock(fd, 1, argv[3]);
+		mmc_change_lock(fd, 1, password, len);
 	} else if (strcmp(argv[1], "unlock") == 0) {
 		printf("trying to unlock...\n");
-		mmc_change_lock(fd, 0, argv[3]);
+		mmc_change_lock(fd, 0, password, len);
 	} else {
 		printf("Unknown parameter\n");
 	}
